b78b4912946eb95c3264244c0eae0952c5f9d060
woerthnicolas <36228626+woerthnicolas@users.noreply.github.com>
2021-10-04 20:42:44 +0200

    Event-based principles with UMG (Health Change Events)

--- Source/ActionRoguelike/Private/SAttributeComponent.cpp
+++ Source/ActionRoguelike/Private/SAttributeComponent.cpp
@@ -13,5 +13,7 @@ bool USAttributeComponent::ApplyHealthChange(float Delta)
 {
 	Health += Delta;
 
+	OnHealthChanged.Broadcast(nullptr, this, Health, Delta);
+
 	return true;
 }
--- Source/ActionRoguelike/Public/SAttributeComponent.h
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -7,6 +7,8 @@
 #include "SAttributeComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHealthChanged, AActor*, InstigatorActor, USAttributeComponent*, OwningComp, float, NewHealth, float, Delta);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
 {
@@ -33,6 +35,9 @@ protected:
 
 public:
 
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthChanged OnHealthChanged;
+	
 	UFUNCTION(BlueprintCallable, Category = "Attributes")
 	bool ApplyHealthChange(float Delta);
 
