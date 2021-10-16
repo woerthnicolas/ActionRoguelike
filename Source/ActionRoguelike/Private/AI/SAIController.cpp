8e17ad85a912cdf371b99eda069bc897d28b89f3
woerthnicolas <36228626+woerthnicolas@users.noreply.github.com>
2021-10-16 10:15:00 +0200

    Adding "Sight" with Pawn Sensing Component

--- Source/ActionRoguelike/Private/AI/SAIController.cpp
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -12,12 +12,12 @@ void ASAIController::BeginPlay()
 
 	RunBehaviorTree(BehaviorTree);
 
-	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
-
-	if(MyPawn)
-	{
-		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
-
-		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
-	}
+	// APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	//
+	// if(MyPawn)
+	// {
+	// 	GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	//
+	// 	GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	// }
 }
